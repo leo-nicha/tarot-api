359981b1145845b58ca98c7d2eff78bd8b1e2c5b
leo-nicha <nicha_bz@hotmail.co.th>
2025-10-24 01:56:45 +0700

    fixed card option

--- api/cards.js
+++ api/cards.js
@@ -1,7 +1,13 @@
-import fs from "fs";
+const fs = require("fs");
 
 export default function handler(req, res) {
-  const data = JSON.parse(fs.readFileSync("tarot_deck_78.json", "utf-8"));
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(data.cards);
+  try {
+    const json = fs.readFileSync("tarot_deck_78.json", "utf-8");
+    const data = JSON.parse(json);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data.cards);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error", details: error.message });
+  }
 }
